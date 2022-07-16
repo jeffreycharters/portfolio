7908543867a2f579f85b37e4cf0a61ad64a6d170
Jeffrey Charters <jeff.charters@gmail.com>
2022-07-16 12:37:19 -0400

    update about me icon

--- src/lib/components/AboutMe.svelte
+++ src/lib/components/AboutMe.svelte
@@ -14,7 +14,7 @@
 	<div class="flex items-center font-semibold px-4">
 		<svg
 			xmlns="http://www.w3.org/2000/svg"
-			class="icon icon-tabler icon-tabler-mood-confuzed mr-2 text-slate-700"
+			class="icon icon-tabler icon-tabler-mood-happy mr-2 text-slate-700"
 			width="24"
 			height="24"
 			viewBox="0 0 24 24"
@@ -24,12 +24,11 @@
 			stroke-linecap="round"
 			stroke-linejoin="round"
 		>
-			<desc>Download more icon variants from https://tabler-icons.io/i/mood-confuzed</desc>
 			<path stroke="none" d="M0 0h24v24H0z" fill="none" />
 			<circle cx="12" cy="12" r="9" />
-			<line x1="9" y1="10" x2="9.01" y2="10" />
-			<line x1="15" y1="10" x2="15.01" y2="10" />
-			<path d="M9.5 16a10 10 0 0 1 6 -1.5" />
+			<line x1="9" y1="9" x2="9.01" y2="9" />
+			<line x1="15" y1="9" x2="15.01" y2="9" />
+			<path d="M8 13a4 4 0 1 0 8 0m0 0h-8" />
 		</svg>
 		<span class="text-slate-800 text-xl">About Me</span>
 	</div>
